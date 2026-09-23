10647cee1bf200163951d62244855f1436bce71c
Petar Petrov <pgpetrov2001@gmail.com>
2026-09-23 15:22:31 +0300

    feat: added timeago pipe from npm package

--- github-analyzer/frontend/src/app/app.config.ts
+++ github-analyzer/frontend/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideTimeago } from 'ngx-timeago';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
+    provideTimeago(),
   ]
 };
--- github-analyzer/frontend/src/app/github-user/github-user.ts
+++ github-analyzer/frontend/src/app/github-user/github-user.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { GithubUserModel } from './github-user.model';
 import { GithubRepo } from '../github-repo/github-repo';
 import { LanguageIcon } from '../language-icon/language-icon';
+import {TimeagoPipe} from "ngx-timeago";
 
 @Component({
   selector: 'app-github-user',
-  imports: [GithubRepo, LanguageIcon],
+  imports: [GithubRepo, LanguageIcon, TimeagoPipe],
   templateUrl: './github-user.html',
   styleUrl: './github-user.css',
 })
